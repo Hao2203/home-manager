db1c9da04e6eb1a17399a1538480eaf7a86fdece
黄豪 <9658583+huanghaodekongjian@user.noreply.gitee.com>
2026-01-22 10:37:31 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,2 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-vim.opt.runtimepath:prepend(lazypath)
-
-require("lazy").setup({
-  lockfile = vim.fn.stdpath("state") .. "/lazy-lock.json",
-})
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
